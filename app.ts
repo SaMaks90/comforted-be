b0d4dc8f1fcece50491df12ee4ffa5c82611009a
SaMaks90 <samchenkoms@gmail.com>
2023-11-21 18:40:19 +0200

    Initial commit

---
+++ app.ts
@@ -0,0 +1,37 @@
+// const cors = require('cors');
+// const express = require('express');
+// const contactsRouter= require('./routes/api/contacts');
+// const authRouter = require('./routes/api/auth');
+// const logger = require('morgan');
+//
+// const app = express();
+//
+// app.use(cors());
+// app.use(express.json());
+// app.use(logger('dev'));
+// app.use(express.static('public'));
+//
+// app.get('/', (req, res, next) => {
+//     res.json({ message: 'CORS is activated'});
+// });
+//
+// app.use('/api/auth', authRouter);
+// app.use('/api/contacts', contactsRouter);
+//
+// app.use((_, res, __) => {
+//     res.status(404).json({
+//         status: 'error',
+//         code: 404,
+//         message: 'Use api on routes: /api/contacts || /api/auth',
+//         data: 'Not found',
+//     });
+// });
+//
+// app.use((err, req, res, next) => {
+//     const { status = 500, message = 'Server error' } = err;
+//     res.status(status).json({
+//         message
+//     });
+// });
+//
+// module.exports = app;
---
+++ server.ts
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { PORT = 3000, DB_HOST } = process.env;
+
+const app = require('./app');
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+    .then(() => {
+        console.log('Database connection successful');
+        app.listen(PORT);
+    })
+    .catch(e => {
+        console.log(e.message);
+        process.exit(1);
+    });
